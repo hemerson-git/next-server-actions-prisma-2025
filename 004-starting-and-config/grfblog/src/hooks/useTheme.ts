46c2a2a6e019dc8b70740eb00e779f14e85077b0
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2025-06-15 19:21:26 -0300

    feat: add useTheme hook

---
+++ 004-starting-and-config/grfblog/src/hooks/useTheme.ts
@@ -0,0 +1,34 @@
+import { useThemeStore } from "@/stores/themeStore";
+
+export const useTheme = () => {
+  const { theme, setTheme } = useThemeStore();
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+
+    document.body.classList.remove(theme);
+    document.body.classList.add(newTheme);
+  };
+
+  const getSavedTheme = () => {
+    const savedTheme = localStorage.getItem("theme");
+
+    if (savedTheme === "dark" || savedTheme === "light") {
+      setTheme(savedTheme);
+    }
+
+    if (!savedTheme) {
+      return document.body.classList.add("light");
+    }
+
+    document.body.classList.add(savedTheme);
+  };
+
+  return {
+    toggleTheme,
+    getSavedTheme,
+  };
+};
